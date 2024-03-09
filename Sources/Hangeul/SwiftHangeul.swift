6eca0eba5998056125841e30200544da34c04db3
pHH <gudghks56@gmail.com>
2024-03-10 01:48:38 +0900

    feat: 외부에서 사용할 Swifthanguel추가

--- Sources/Hangeul/SwiftHangeul.swift
+++ Sources/Hangeul/SwiftHangeul.swift
@@ -13,15 +13,44 @@ import UIKit
 
 #endif
 
-
-
-
-class SwiftHangeul{
+public final class SwiftHangeul {
     
-    init(){
-        
+    private var factory: HangeulFactory
+    private var hanguelState: Hangule
+    
+    public init(factory: HangeulFactory = .shared, hanguelState: Hangule = Hangule()) {
+        self.factory = factory
+        self.hanguelState = hanguelState
+    }
+    
+    public func input(_ str: String?) {
+        if let str
+        {
+            let array = factory.글자_분해_함수(input: str)
+            array.forEach { char in
+                hanguelState.inputLetter(char)
+            }
+        }
+        else 
+        {
+            hanguelState.inputLetter(nil)
+        }
+    }
+    
+    public func separate(input: String) -> [String] {
+        let array = factory.글자_분해_함수(input: input)
+        return array.map { String($0) }
+    }
+    
+    public func backKey() {
+        hanguelState.inputLetter(nil)
+    }
+    
+    public func getTotoal() -> String {
+        hanguelState.getTotalString()
     }
 
+    
 }
 
 
