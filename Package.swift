f1cfcb96c1bbeebf62bf8ccfcc9d411092614d59
pHH <gudghks56@gmail.com>
2024-03-10 15:15:31 +0900

    rename: Package name 변경

--- Package.swift
+++ Package.swift
@@ -4,23 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "Hangeul",
+    name: "SwiftHangeul",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Hangeul",
-            targets: ["Hangeul"]),
+            name: "SwiftHangeul",
+            targets: ["SwiftHangeul"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Hangeul"),
+            name: "SwiftHangeul"),
         .testTarget(
-            name: "HangeulTests",
-            dependencies: ["Hangeul"]),
+            name: "SwiftHangeulTests",
+            dependencies: ["SwiftHangeul"]),
     ]
 )
