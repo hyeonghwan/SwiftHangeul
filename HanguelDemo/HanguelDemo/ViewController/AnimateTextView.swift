5f16aa5c5a9421d2d75436336c5aff638ddb4f65
pHH <gudghks56@gmail.com>
2024-05-01 16:39:01 +0900

    feat: AnimateText View 파일 분리, editable false

--- HanguelDemo/HanguelDemo/ViewController/AnimateTextView.swift
+++ HanguelDemo/HanguelDemo/ViewController/AnimateTextView.swift
@@ -1,16 +1,14 @@
+//
+//  AnimateTextView.swift
+//  HanguelDemo
+//
+//  Created by 박형환 on 5/1/24.
+//
 
-
-import Combine
 import SwiftHangeul
 import UIKit
 
-public enum AnimateHangeul {
-    case start
-    case progress(Character)
-    case finish
-}
-
-open class AnimateTextLabel: UILabel, AnimateTextType {
+open class AnimateTextView: UITextView, AnimateTextType {
     public var id: UUID = UUID()
     open var seconds: TimeInterval!
     open var animateText: String!
@@ -22,18 +20,18 @@ open class AnimateTextLabel: UILabel, AnimateTextType {
     
     public convenience init(frame: CGRect = .zero,
                             seconds: TimeInterval = 0.05,
-                            text: String,
+                            text: String  = "안녕하세요 저는 박형환 이라고 합니다.",
                             gesture enable: Bool = true)
     {
-        self.init(frame: frame)
+        self.init(frame: frame, textContainer: nil)
         self.seconds = seconds
         self.animateText = text
         self.defaultText = text
         applyAttribute(enable: enable)
     }
     
-    private override init(frame: CGRect) {
-        super.init(frame: frame)
+    public override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
     }
     
     public required init?(coder: NSCoder) {
@@ -41,8 +39,10 @@ open class AnimateTextLabel: UILabel, AnimateTextType {
     }
     
     open func applyAttribute(enable: Bool) {
-        self.numberOfLines = 0
+        self.layer.borderColor = UIColor.white.cgColor
+        self.layer.borderWidth = 1
         self.isUserInteractionEnabled = enable
+        self.isEditable = false
     }
     
     public func cacheClear() {
