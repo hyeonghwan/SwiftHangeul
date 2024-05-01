7ffa23cd7f821e2bb331b9a235f2a64a00c14d51
pHH <gudghks56@gmail.com>
2024-05-01 21:36:48 +0900

    feat: TableView Cell Add

--- HanguelDemo/HanguelDemo/View/TableViewCell.swift
+++ HanguelDemo/HanguelDemo/View/TableViewCell.swift
@@ -11,19 +11,40 @@ import UIKit
 protocol CellIdentifier { }
 
 extension CellIdentifier {
-    var identifier: String {
+    static var identifier: String {
         String(describing: Self.self)
     }
 }
 
 final class TableViewCell: UITableViewCell, CellIdentifier {
     
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 20)
+        label.textColor = .label
+        return label
+    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addAutoLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("required init fatalError")
+    }
+    
+    func apply(text: String) {
+        self.label.text = text
+    }
+    
+    private func addAutoLayout() {
+        self.contentView.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor,constant: 20),
+            label.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor)
+        ])
     }
 }
